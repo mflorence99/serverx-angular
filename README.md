5e21f1199588bf7c774b518a35ea3a096f960b60
Mark Florence <mflo999@gmail.com>
2019-01-12 16:29:19 -0500

    Kinda working for AWS, not GCF

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # serverx-angular
 Deploy Angular application to serverless environments using ServeRX-ts
+
+## temp
+* https://v1pzo1e2d1.execute-api.us-east-1.amazonaws.com/dev
+* https://us-east1-gcf-project-45679.cloudfunctions.net/gcf
--- src/functions.ts
+++ src/functions.ts
@@ -2,7 +2,9 @@ import 'reflect-metadata';
 
 import { AWSLambdaApp } from 'serverx-ts';
 import { Compressor } from 'serverx-ts';
+import { COMPRESSOR_OPTS } from 'serverx-ts';
 import { CORS } from 'serverx-ts';
+import { FILE_SERVER_OPTS } from 'serverx-ts';
 import { FileServer } from 'serverx-ts';
 import { GCFApp } from 'serverx-ts';
 import { Route } from 'serverx-ts';
@@ -12,7 +14,11 @@ const routes: Route[] = [
   {
     path: '/',
     handler: FileServer,
-    middlewares: [Compressor, CORS]
+    middlewares: [Compressor, CORS],
+    services: [
+      { provide: COMPRESSOR_OPTS, useValue: { threshold: 0 } }, 
+      { provide: FILE_SERVER_OPTS, useValue: { root: __dirname } }
+    ]
   }
 
 ];
